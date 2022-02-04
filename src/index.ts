77ccf76180b0dea3d7797a72d5421237a1a1c78b
Frantz Crozilhac <fran.stacksta@gmail.com>
2022-02-04 18:46:06 +0100

    User - Fetch Functionality

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,30 @@
 import { User } from './models/User'
+import axios from 'axios'
 
-const user = new User({name: 'test', age: 20});
+// const user = new User({name: 'test', age: 20});
+// user.on('click',()=>{
+//     console.log('hey')
+// user.trigger('click')
+// // console.log(user)
 
-user.on('click',()=>{
-    console.log('hey')
-})
-user.on('change',()=>{
-    console.log('mouaaaa')
-})
 
-user.trigger('click')
-user.trigger('blop')
-user.trigger('change')
-// console.log(user)
+// axios.post('http://localhost:3000/users',{
+//     name: 'test',
+//     age: 20
+// })
+
+// axios({
+//     method: 'post',
+//     url: 'http://locallhost:3000/users',
+//     headers: { 
+//         'Access-Control-Allow-Origin': '*',
+//         'Content-Type': 'application/json' 
+//     },
+//     data: {
+//         name: 'test',
+//         age: 20
+//     }
+//   });
+
+const user = new User({id: 1});
+user.fetch()
