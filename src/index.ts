3507eb09afc87115143efb1c1b7cb1c737a8b56a
Frantz Crozilhac <fran.stacksta@gmail.com>
2022-02-04 14:23:29 +0100

    User - #Setting Properties

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,8 @@ const user = new User({name: ' test', age: 20});
 
 console.log(user.get('name'))
 console.log(user.get('age'))
+
+user.set({name:'john'})
+
+console.log(user.get('name'))
+console.log(user.get('age'))
