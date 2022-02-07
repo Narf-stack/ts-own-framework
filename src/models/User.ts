9c7eecb43c6c61943f7d3bbc6c61a75f5d7785f8
Frantz Crozilhac <fran.stacksta@gmail.com>
2022-02-07 23:25:20 +0100

    User - Class Method for Collections

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,6 +2,7 @@ import { Eventing } from './Eventing'
 import { ApiSync } from './ApiSync';
 import { Attributes } from './Attributes';
 // import { AxiosResponse } from 'axios';
+import { Collection } from './Collection';
 import { Model } from "./Model"
 
 export interface UserProps{
@@ -26,6 +27,12 @@ export class User extends Model<UserProps> {
             new Attributes<UserProps>(attrs),
             new Eventing(),
             new ApiSync<UserProps>(rootUrl)
+        )
+    }
+
+    static buildUserCollection():Collection<User,UserProps>{
+        return new Collection<User,UserProps>(rootUrl,
+            (json: UserProps)=> User.buildUser(json)
         )
     }
 }
